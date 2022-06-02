88e57cb6a9db9df84f3ddd2bc98dc23dd996e788
Harrison Pielke-Lombardo <harrison.pielke-lombardo@cuanschutz.edu>
2022-06-02 14:08:08 -0600

    Implemented tests.

--- src/tuh8888/datascript_shortest_path.clj
+++ src/tuh8888/datascript_shortest_path.clj
@@ -76,100 +76,23 @@
                            (constantly Integer/MAX_VALUE))
                   queue)]))))
 
+(defn nodes
+  [g]
+  (-> g
+      (igraph/query '[:find ?node-id
+                      :where (or [?node :to] [_ :target ?node])
+                      [?node ::dsg/id ?node-id]])
+      (->> (map :?node-id))))
+
 (defn shortest-path
   [g node &
    {:keys [neighbor-fn weight-fn]
     :or   {neighbor-fn get-neighbors
            weight-fn   get-weight}}]
-  (-> g
-      (igraph/query '[:find ?node-id
-                      :where (or [?node :to] [_ :target ?node])
-                      (not [?node ::dsg/id :a])
-                      [?node ::dsg/id ?node-id]])
-      (->> (map :?node-id)
-           (cons node)
-           (igraph/traverse g
-                            (dijkstra-shortest-path-traversal neighbor-fn
-                                                              weight-fn)
-                            {::paths {node {::dist 0}}}
-                            {}))))
-
-(comment
-  (let [g (-> {:weight {:db/type :db.type/integer}}
-              (dsg/make-graph)
-              (igraph/add (complex-triples [[:a
-                                             :to
-                                             {:b {:weight 1}
-                                              :c {:weight 1}
-                                              :d {:weight 1}}]
-                                            [:b
-                                             :to {:e {:weight 2}}]])))]
-    (-> g
-        (shortest-path :a)
-        (=
-         {:a {::dist  0
-              ::nodes [:a]}
-          :b {::dist  1
-              ::nodes [:a :b]}
-          :c {::dist  1
-              ::nodes [:a :c]}
-          :d {::dist  1
-              ::nodes [:a :d]}
-          :e {::dist  3
-              ::nodes [:a :b :e]}}))))
-
-(comment
-  (let [g (-> {:weight {:db/type :db.type/integer}}
-              (dsg/make-graph)
-              (igraph/add
-               (complex-triples [[:a
-                                  :to
-                                  {:c {:weight 3}
-                                   :d {:weight 7}
-                                   :e {:weight 5}}]
-                                 [:c
-                                  :to
-                                  {:a {:weight 3}
-                                   :f {:weight 7}
-                                   :d {:weight 1}}]
-                                 [:d
-                                  :to
-                                  {:a {:weight 7}
-                                   :c {:weight 1}
-                                   :f {:weight 2}
-                                   :g {:weight 1}
-                                   :h {:weight 3}
-                                   :e {:weight 3}}]
-                                 [:e
-                                  :to
-                                  {:a {:weight 5}
-                                   :d {:weight 3}
-                                   :h {:weight 2}}]
-                                 [:f
-                                  :to
-                                  {:c {:weight 7}
-                                   :d {:weight 2}
-                                   :g {:weight 2}
-                                   :i {:weight 1}}]
-                                 [:g :to
-                                  {:d {:weight 1}
-                                   :f {:weight 2}
-                                   :h {:weight 3}
-                                   :i {:weight 3}
-                                   :b {:weight 2}}]
-                                 [:h
-                                  :to
-                                  {:d {:weight 3}
-                                   :e {:weight 2}
-                                   :g {:weight 3}
-                                   :b {:weight 4}}]
-                                 [:i
-                                  :to
-                                  {:f {:weight 1}
-                                   :g {:weight 3}
-                                   :b {:weight 5}}]])))]
-    (-> g
-        (shortest-path :a)
-        :b
-        (= {::dist  7
-            ::nodes [:a :c :d :g :b]}))))
+  (->> g
+       nodes
+       (cons node)
+       (igraph/traverse g
+                        (dijkstra-shortest-path-traversal neighbor-fn weight-fn)
+                        {::paths {node {::dist 0}}}
+                        {})))
--- test/tuh8888/datascript_shortest_path_test.clj
+++ test/tuh8888/datascript_shortest_path_test.clj
@@ -1,7 +1,83 @@
 (ns tuh8888.datascript-shortest-path-test
-  (:require [clojure.test :refer :all]
-            [tuh8888.datascript-shortest-path :refer :all]))
+  (:require
+   [clojure.test :refer [deftest testing is]]
+   [ont-app.datascript-graph.core :as dsg]
+   [ont-app.igraph.core :as igraph]
+   [tuh8888.datascript-shortest-path :as sut]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest shortest-path-test
+  (let [g (-> {:weight {:db/type :db.type/integer}}
+              (dsg/make-graph)
+              (igraph/add (sut/complex-triples [[:a
+                                                 :to
+                                                 {:b {:weight 1}
+                                                  :c {:weight 1}
+                                                  :d {:weight 1}}]
+                                                [:b
+                                                 :to {:e {:weight 2}}]])))]
+    (is (= {:a {::sut/dist  0
+                ::sut/nodes [:a]}
+            :b {::sut/dist  1
+                ::sut/nodes [:a :b]}
+            :c {::sut/dist  1
+                ::sut/nodes [:a :c]}
+            :d {::sut/dist  1
+                ::sut/nodes [:a :d]}
+            :e {::sut/dist  3
+                ::sut/nodes [:a :b :e]}}
+           (-> g
+               (sut/shortest-path :a)))))
+  ;; From https://brilliant.org/wiki/dijkstras-short-path-finder/#examples
+  (let [g (-> {:weight {:db/type :db.type/integer}}
+              (dsg/make-graph)
+              (igraph/add (sut/complex-triples [[:a
+                                                 :to
+                                                 {:c {:weight 3}
+                                                  :d {:weight 7}
+                                                  :e {:weight 5}}]
+                                                [:c
+                                                 :to
+                                                 {:a {:weight 3}
+                                                  :f {:weight 7}
+                                                  :d {:weight 1}}]
+                                                [:d
+                                                 :to
+                                                 {:a {:weight 7}
+                                                  :c {:weight 1}
+                                                  :f {:weight 2}
+                                                  :g {:weight 1}
+                                                  :h {:weight 3}
+                                                  :e {:weight 3}}]
+                                                [:e
+                                                 :to
+                                                 {:a {:weight 5}
+                                                  :d {:weight 3}
+                                                  :h {:weight 2}}]
+                                                [:f
+                                                 :to
+                                                 {:c {:weight 7}
+                                                  :d {:weight 2}
+                                                  :g {:weight 2}
+                                                  :i {:weight 1}}]
+                                                [:g :to
+                                                 {:d {:weight 1}
+                                                  :f {:weight 2}
+                                                  :h {:weight 3}
+                                                  :i {:weight 3}
+                                                  :b {:weight 2}}]
+                                                [:h
+                                                 :to
+                                                 {:d {:weight 3}
+                                                  :e {:weight 2}
+                                                  :g {:weight 3}
+                                                  :b {:weight 4}}]
+                                                [:i
+                                                 :to
+                                                 {:f {:weight 1}
+                                                  :g {:weight 3}
+                                                  :b {:weight 5}}]])))]
+    (is (= {::sut/dist  7
+            ::sut/nodes [:a :c :d :g :b]}
+           (-> g
+               (sut/shortest-path :a)
+               :b)))))
